2e293cde64a566e00a4f252abac85fe0ed9f9dd5
DevData <83123414+StoneOceans@users.noreply.github.com>
2024-06-28 18:52:32 +0200

    Update test_algo_b_2_4_bis_traitement_unitaire.py

--- test2/test_algo_b_2_4_bis_traitement_unitaire.py
+++ test2/test_algo_b_2_4_bis_traitement_unitaire.py
@@ -14,9 +14,6 @@ from src.stan import read_and_process_file, convert_and_calculate
 from src.algo_b_2_3_traitement_preliminaire import traitement_utile_inutile
 from src.algo_b_2_4_bis_traitement_unitaire import traitement_unitaire
 
-result = read_and_process_file("data/RDVC-20230522.pln")
-result = convert_and_calculate(result)
-result = traitement_utile_inutile(result)
 result_utile = result[result["utile_inutile"] == "UTI"].copy()
 result_utile_traitee = traitement_unitaire(result_utile)
 
