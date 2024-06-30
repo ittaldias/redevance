9879f13164292d0c24ca5b580f3c2a901c187e4d
DevData <83123414+StoneOceans@users.noreply.github.com>
2024-06-30 17:26:09 +0200

    Delete test2/script_to_process_data.py

--- test2/script_to_process_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import pytest
-from datetime import datetime, timedelta
-from src.stan import read_and_process_file, convert_and_calculate
-
-output = read_and_process_file("data/RDVC-20230522.pln")
-processed_output = convert_and_calculate(output)
-processed_output.to_pickle('processed_data.pkl')  # Save processed data to a file
